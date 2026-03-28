18c3b246fda6fa0141ce30ecb3f9a932f2b398b7
ti1l <162818384+ti1l@users.noreply.github.com>
2026-03-28 12:19:40 +0400

    Add RegistrationStatus enum for user sessions
    Define an enum for user registration statuses.

---
+++ enums.dart
@@ -0,0 +1,6 @@
+//The possible status of registrationfor a user in a session 
+enum RegistrationStatus { 
+  confirmed, //The user registeres but is waiting for confirmation 
+  pending, //Registration have been accepted
+  cancelled //Registration have been cancelled 
+  }
